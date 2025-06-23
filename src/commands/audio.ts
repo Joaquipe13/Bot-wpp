9e2c1675b86325a44663b608bbcac4a9051fbaf5
Joaquin Carlos Fernandez Da Silva <jfernandezd@frro.utn.edu.ar>
2025-06-23 19:08:05 -0300

    Refactor audioCommand function for improved error handling and code clarity

--- src/commands/audio.ts
+++ src/commands/audio.ts
@@ -3,16 +3,23 @@ import fs from 'fs';
 import path from 'path';
 import {  MessageMedia } from 'whatsapp-web.js';
 
-export async function audioCommand(file:string) {
+export async function audioCommand(file: string) {
   const carpeta = path.join(__dirname, './../../audios/', file);
-  const archivos = fs.readdirSync(carpeta).filter(file => file.endsWith('.opus'));
+
+  if (!fs.existsSync(carpeta)) {
+    throw new Error(`❌ La carpeta ${file} no existe.`);
+  }
+
+  const archivos = fs.readdirSync(carpeta).filter(f => f.endsWith('.opus'));
   if (archivos.length === 0) {
     throw new Error(`❌ No hay audios del ${file} disponibles.`);
   }
+
   const archivoElegido = archivos[Math.floor(Math.random() * archivos.length)];
   const ruta = path.join(carpeta, archivoElegido);
   const buffer = fs.readFileSync(ruta);
   const base64Audio = buffer.toString('base64');
-  const media = new MessageMedia('audio/ogg; codecs=opus', base64Audio, `${file}.opus`);
-  return media
+  const media = new MessageMedia('audio/ogg; codecs=opus', base64Audio, archivoElegido);
+  return media;
 }
+
