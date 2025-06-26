8e8351c6c944c557baac8b12fbb5160eb543383e
Joaquin Carlos Fernandez Da Silva <jfernandezd@frro.utn.edu.ar>
2025-06-26 13:40:54 -0300

    Refactor project structure by deleting unnecessary files and directories; update .gitignore to exclude dist and source files; enhance DatabaseManager with reconnectPool and query methods for better connection handling; add sourceMap and incremental options to tsconfig.json.

--- dist/bot/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerClientEvents = exports.createClient = void 0;
-var initClient_1 = require("./initClient");
-Object.defineProperty(exports, "createClient", { enumerable: true, get: function () { return initClient_1.createClient; } });
-var events_1 = require("./events");
-Object.defineProperty(exports, "registerClientEvents", { enumerable: true, get: function () { return events_1.registerClientEvents; } });
--- dist/commands/ping.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pingCommand = void 0;
-const pingCommand = (msg) => {
-    msg.reply("🏓 Pong!");
-};
-exports.pingCommand = pingCommand;
